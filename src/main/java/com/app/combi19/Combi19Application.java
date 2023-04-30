d70dd998d53b57df3d20e0a23ebc4752ccb77e76
juan <juanfernandez_carc@hotmail.com>
2023-04-30 18:44:18 -0300

    Project creation with Spring Boot

---
+++ src/main/java/com/app/combi19/Combi19Application.java
@@ -0,0 +1,12 @@
+package com.app.combi19;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Combi19Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Combi19Application.class, args);
+	}
+}
---
+++ src/test/java/com/app/combi19/Combi19ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.combi19;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Combi19ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
